948fda690e006de401f42d8f4a4ae40eaf2e780f
pooya_dehghan <pooyadehghan@yahoo.com>
2023-07-12 22:10:24 +0330

    initializing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootPage from './pages/root';
+import Dashboard from './pages/Dashboard';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootPage />}>
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Route>
+  )
+);
+const rootEl = document.getElementById("root") as HTMLElement
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
---
+++ frontend/src/pages/root.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { makeStyles } from '@material-ui/styles';
+import axios from 'axios';
+
+const useStyles = makeStyles({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+  },
+  loginHeader: {
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    textAlign: 'center',
+  },
+  textAreaContainer: {
+    textAlign: 'center',
+  },
+});
+
+const Root = () => {
+  const classes = useStyles();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const loginRequest = () => {
+    console.log('login');
+    axios
+      .post('localhost:8000/user', {
+        firstName: username,
+        lastName: password,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Grid container spacing={2} className={classes.container}>
+      <Grid item xs={12} className={classes.loginHeader}>
+        LOGIN
+      </Grid>
+      <Grid item xs={12} className={classes.textAreaContainer}>
+        <TextField
+          onChange={(e) => setUsername(e.target.value)}
+          id="outlined-basic"
+          label="نام کاربری"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12} className={classes.textAreaContainer}>
+        <TextField
+          onChange={(e) => setPassword(e.target.value)}
+          id="outlined-basic"
+          label="گذرواژه"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={8} className={classes.buttonContainer}>
+        <Button onClick={() => loginRequest()} variant="contained">
+          ورود
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Root;
