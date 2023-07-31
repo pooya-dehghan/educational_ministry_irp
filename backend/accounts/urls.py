f6095baee67c8ba7ff1741d369df452ddeb4a790
alishamsfi80 <lshms3496@gmail.com>
2023-07-30 23:50:24 -0700

    delete before code

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,12 +4,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("api/v1/", include("accounts.api.V1.urls", namespace="api-V1")),
-    path('officemanager/', views.OfficeManagerView.as_view()),
-    path('officemanager/<int:pk>/', views.OfficeManagerView.as_view()),
-    path('professor/', views.ProfessorView.as_view()),
-    path('professor/<int:pk>/', views.ProfessorView.as_view()),
-    path('teacher/', views.TeacherView.as_view()),
-    path('teacher/<int:pk>/', views.TeacherView.as_view()),
 
 
 ]
