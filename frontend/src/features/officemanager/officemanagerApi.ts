0cbf3fe64408c9635ce915a4824bbf4ca68ee75c
pooya_dehghan <pooyadehghan@yahoo.com>
2023-08-11 16:22:06 +0330

    added new model features redux

---
+++ frontend/src/features/officemanager/officemanagerApi.ts
@@ -0,0 +1,86 @@
+import axios from 'axios';
+import {
+  CreateOfficeManagerRequest,
+  CreateOfficeManagerResponse,
+  DeleteOfficeManagereRequest,
+  DeleteOfficeManagerResponse,
+  GetAllOfficeManagersRequest,
+  GetAllOfficeManagersResponse,
+  GetOfficeManagerRequest,
+  GetOfficeManagerResponse,
+  UpdateOfficeManagerRequest,
+  UpdateOfficeManagerResponse,
+} from './interface/interface.index';
+const API_BASE_URL = 'http://localhost:8000'; // Your API base URL
+
+export const officeManagerApi = {
+  getOfficeManager: async (
+    getData: GetOfficeManagerRequest
+  ): Promise<GetOfficeManagerResponse> => {
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}/accounts/api/v1/officemanager/`,
+        getData
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  createOfficeManager: async (
+    createData: CreateOfficeManagerRequest
+  ): Promise<CreateOfficeManagerResponse> => {
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}/accounts/api/v1/officemanager/`,
+        createData
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  updateOfficeManager: async (
+    updateData: UpdateOfficeManagerRequest
+  ): Promise<UpdateOfficeManagerResponse> => {
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}/accounts/api/v1/officemanager/`,
+        updateData
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  deleteOfficeManager: async (
+    deleteData: DeleteOfficeManagereRequest
+  ): Promise<DeleteOfficeManagerResponse> => {
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}/accounts/api/v1/officemanager/`,
+        deleteData
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getAllOfficeManagers: async (
+    getData: GetAllOfficeManagersRequest
+  ): Promise<GetAllOfficeManagersResponse> => {
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}/accounts/api/v1/officemanager/`,
+        getData
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
