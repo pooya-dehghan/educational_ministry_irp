662db89be903cd1a3672e004c90974f87cd5a53f
PouryaCE <PouryaJanparvar@yahoo.com>
2023-08-17 14:43:39 +0430

    small updated commit

--- backend/officemanager/api/V1/views.py
+++ backend/officemanager/api/V1/views.py
@@ -5,7 +5,7 @@ from .serializers import OfficeManagerSerializer, SchoolListSerializer, SchoolSe
 from rest_framework import status
 from .permissions import IsSuperuser, IsSuperuserOrOwnOfficeManager
 from drf_yasg.utils import swagger_auto_schema
-from .swagger_info import swagger_parameters
+from .swagger_info import swagger_parameters, swagger_docs
 
 
 class OfficeManagerGet(APIView):
@@ -30,7 +30,11 @@ class OfficeManagerCreate(APIView):
     permission_classes = [IsSuperuser]
 
     @swagger_auto_schema(
+<<<<<<< Updated upstream
         manual_parameters=swagger_parameters
+=======
+            manual_parameters=swagger_parameters,
+>>>>>>> Stashed changes
     )
     def post(self, request):
         ser_data = OfficeManagerSerializer(data=request.POST)
