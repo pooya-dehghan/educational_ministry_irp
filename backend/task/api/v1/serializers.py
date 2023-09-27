f9fd8d70f5fafc3396f40fc9fc5f83ce3b06136d
PouryaCE <pouryahanparvar@yahoo.com>
2023-09-27 22:30:17 +0330

    upload task by student

--- backend/task/api/v1/serializers.py
+++ backend/task/api/v1/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from ...models import Task
+from ...models import Task, RenderTask
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         exclude = ("professor",)
+
+
+class RenderTaskUploadingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RenderTask
+        fields = ("file",)
--- backend/task/api/v1/views.py
+++ backend/task/api/v1/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from accounts.models import Professor, Student
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
-from ...models import Task
-from .serializers import TaskSerializer
+from ...models import Task, RenderTask
+from .serializers import TaskSerializer, RenderTaskUploadingSerializer
 from notification.models import Notification
 
 
@@ -25,7 +25,33 @@ class TaskCreationView(APIView):
             )
             students = Student.objects.filter(professor2=professor)
             for student in students:
-                Notification.objects.create(sender=request.user, receiver=student, code=300, title=task.title, body=f"استاد {professor.username} تکیف جدیدی به نام {task.title}ایجاد نموده است و تا تاریخ {task.deadline}فرصت دارید تا آن را انجام دهید.")
-            return Response({"message":"تکلیف شما با موفقیت ساخته شد و برای دانش آموزان اطلاعیه ارسال شد"}, status=status.HTTP_201_CREATED)
+                Notification.objects.create(sender=request.user, receiver=student, code=300, title=task.title,
+                                            body=f"استاد {professor.username} تکیف جدیدی به نام {task.title}ایجاد نموده است و تا تاریخ {task.deadline}فرصت دارید تا آن را انجام دهید.")
+            return Response({"message": "تکلیف شما با موفقیت ساخته شد و برای دانش آموزان اطلاعیه ارسال شد"},
+                            status=status.HTTP_201_CREATED)
         else:
-            return Response({"data":ser_data.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"data": ser_data.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RenderTaskCreationView(APIView):
+    def post(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        try:
+            student = Student.objects.get(id=request.user.id)
+        except ObjectDoesNotExist:
+            return Response({"data": "invalid student"}, status=status.HTTP_400_BAD_REQUEST)
+
+        ser_data = RenderTaskUploadingSerializer(data=request.data)
+        if ser_data.is_valid():
+            rt = RenderTask.objects.create(
+                file=ser_data.validated_data["file"],
+                student=student,
+                task=task
+            )
+            Notification.objects.create(sender=student, receiver=student.professor2, code=250, title="انجام تکلیف",
+                                        body=f"دانشجو{student.username} تکلیف {task.title} را در تاریخ {rt.delivery_date} آپلود کرده است")
+
+            return Response({"message": "تکلیف شما با موفقیت آپلود شد"},
+                            status=status.HTTP_201_CREATED)
+        else:
+            return Response({"data": ser_data.errors}, status=status.HTTP_400_BAD_REQUEST)
