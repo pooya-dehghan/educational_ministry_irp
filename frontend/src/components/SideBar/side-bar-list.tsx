1a84eee7402f291b95c700216176440e29b5e493
pooya_dehghan <pooyadehghan@yahoo.com>
2023-10-05 18:45:54 +0330

    side bar has user profile section

--- frontend/src/components/SideBar/side-bar-list.tsx
+++ frontend/src/components/SideBar/side-bar-list.tsx
@@ -1,183 +1,221 @@
-import SchoolIcon from "@mui/icons-material/School";
-import HowToReg from "@mui/icons-material/HowToReg";
-import HomeWorkIcon from "@mui/icons-material/HomeWork";
-import ApartmentIcon from "@mui/icons-material/Apartment";
-import PersonSharpIcon from "@mui/icons-material/PersonSharp";
-import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import SchoolIcon from '@mui/icons-material/School';
+import HowToReg from '@mui/icons-material/HowToReg';
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import PersonSharpIcon from '@mui/icons-material/PersonSharp';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 
 export const ListItems = [
   {
-    list: "لیست مدارس",
+    list: 'لیست مدارس',
     icon: <SchoolIcon />,
-    to: "/list/schools",
+    to: '/list/schools',
   },
   {
-    list: "لیست مسئولین آموزش و پرورش",
+    list: 'لیست مسئولین آموزش و پرورش',
     icon: <PersonSharpIcon />,
-    to: "/list/officemanagers",
+    to: '/list/officemanagers',
   },
   {
-    list: "لیست اساتید",
+    list: 'لیست اساتید',
     icon: <PersonSharpIcon />,
-    to: "/list/professors",
+    to: '/list/professors',
   },
   {
-    list: "لیست دانشجویان",
+    list: 'لیست دانشجویان',
     icon: <PersonSharpIcon />,
-    to: "/list/students",
+    to: '/list/students',
   },
   {
-    list: "لیست دبیران",
+    list: 'لیست دبیران',
     icon: <PersonSharpIcon />,
-    to: "/list/teachers",
+    to: '/list/teachers',
   },
   {
-    list: "افزودن مدرسه",
+    list: 'افزودن مدرسه',
     icon: <SchoolIcon />,
-    to: "/createSchool",
+    to: '/createSchool',
   },
   {
-    list: "افزودن مسئول اداره آموزش و پرورش",
+    list: 'افزودن مسئول اداره آموزش و پرورش',
     icon: <PersonAddAlt1Icon />,
-    to: "/createOfficeManager",
+    to: '/createOfficeManager',
   },
   {
-    list: "افزودن استاد",
+    list: 'افزودن استاد',
     icon: <PersonAddAlt1Icon />,
-    to: "/createProfessor",
+    to: '/createProfessor',
   },
   {
-    list: "افزودن معلم مدرسه",
+    list: 'افزودن معلم مدرسه',
     icon: <PersonAddAlt1Icon />,
-    to: "/createTeacher",
+    to: '/createTeacher',
   },
   {
-    list: "درخواست ها",
+    list: 'درخواست ها',
     icon: <PersonAddAlt1Icon />,
-    to: "/requests",
+    to: '/requests',
   },
 ];
 
-export const appropriateListReturned = (userType: string) => {
+export const appropriateListReturned = (userType: string, id: number) => {
   switch (userType) {
-    case "officemanager":
-      return officeManagerList;
-    case "admin":
-      return adminList;
-    case "superuser":
-      return adminList;
-    case "teacher":
-      return teacherList;
-    case "school":
-      return schoolList;
-    case "professor":
-      return professorList;
-    case "teacher":
-      return teacherList;
+    case 'officemanager':
+      return officeManagerList(userType, id);
+    case 'admin':
+      return adminList(userType, id);
+    case 'superuser':
+      return adminList(userType, id);
+    case 'teacher':
+      return teacherList(userType, id);
+    case 'school':
+      return schoolList(userType, id);
+    case 'professor':
+      return professorList(userType, id);
+    case 'teacher':
+      return teacherList(userType, id);
+    case 'student':
+      return studentList(userType, id);
     default:
-      return studentList;
+      return studentList(userType, id);
   }
 };
 
-export const adminList = [
-  {
-    list: "لیست مدارس",
-    icon: <SchoolIcon />,
-    to: "/list/schools",
-  },
-  {
-    list: "لیست مسئولین آموزش و پرورش",
-    icon: <PersonSharpIcon />,
-    to: "/list/officemanagers",
-  },
-  {
-    list: "لیست اساتید",
-    icon: <PersonSharpIcon />,
-    to: "/list/professors",
-  },
-  {
-    list: "لیست دانشجویان",
-    icon: <PersonSharpIcon />,
-    to: "/list/students",
-  },
-  {
-    list: "افزودن مدرسه",
-    icon: <SchoolIcon />,
-    to: "/createSchool",
-  },
-  {
-    list: "افزودن مسئول اداره آموزش و پرورش",
-    icon: <PersonAddAlt1Icon />,
-    to: "/createOfficeManager",
-  },
-  {
-    list: "افزودن استاد",
-    icon: <PersonAddAlt1Icon />,
-    to: "/createProfessor",
-  },
-  {
-    list: "افزودن معلم مدرسه",
-    icon: <PersonAddAlt1Icon />,
-    to: "/createTeacher",
-  },
-  {
-    list: "درخواست ها",
-    icon: <PersonAddAlt1Icon />,
-    to: "/requests",
-  },
-];
+export const adminList = (usertype: string, id: number) => {
+  return [
+    {
+      list: 'پروفایل کاربری',
+      icon: <PersonAddAlt1Icon />,
+      to: `/${usertype}/${id}`,
+    },
+    {
+      list: 'لیست مدارس',
+      icon: <SchoolIcon />,
+      to: '/list/schools',
+    },
+    {
+      list: 'لیست مسئولین آموزش و پرورش',
+      icon: <PersonSharpIcon />,
+      to: '/list/officemanagers',
+    },
+    {
+      list: 'لیست اساتید',
+      icon: <PersonSharpIcon />,
+      to: '/list/professors',
+    },
+    {
+      list: 'لیست دانشجویان',
+      icon: <PersonSharpIcon />,
+      to: '/list/students',
+    },
+    {
+      list: 'افزودن مدرسه',
+      icon: <SchoolIcon />,
+      to: '/createSchool',
+    },
+    {
+      list: 'افزودن مسئول اداره آموزش و پرورش',
+      icon: <PersonAddAlt1Icon />,
+      to: '/createOfficeManager',
+    },
+    {
+      list: 'افزودن استاد',
+      icon: <PersonAddAlt1Icon />,
+      to: '/createProfessor',
+    },
+    {
+      list: 'افزودن معلم مدرسه',
+      icon: <PersonAddAlt1Icon />,
+      to: '/createTeacher',
+    },
+    {
+      list: 'درخواست ها',
+      icon: <PersonAddAlt1Icon />,
+      to: '/requests',
+    },
+  ];
+};
 
-export const officeManagerList = [
-  {
-    list: "لیست مدارس",
-    icon: <SchoolIcon />,
-    to: "/list/schools",
-  },
-  {
-    list: "لیست دانشجویان",
-    icon: <PersonSharpIcon />,
-    to: "/list/students",
-  },
-  {
-    list: "درخواست ها",
-    icon: <PersonAddAlt1Icon />,
-    to: "/requests",
-  },
-];
+export const officeManagerList = (usertype: string, id: number) => {
+  return [
+    {
+      list: 'پروفایل کاربری',
+      icon: <PersonAddAlt1Icon />,
+      to: `/${usertype}/${id}`,
+    },
+    {
+      list: 'لیست مدارس',
+      icon: <SchoolIcon />,
+      to: '/list/schools',
+    },
+    {
+      list: 'لیست دانشجویان',
+      icon: <PersonSharpIcon />,
+      to: '/list/students',
+    },
+    {
+      list: 'درخواست ها',
+      icon: <PersonAddAlt1Icon />,
+      to: '/requests',
+    },
+  ];
+};
 
-export const schoolList = [
-  {
-    list: "لیست دانشجویان",
-    icon: <PersonSharpIcon />,
-    to: "/list/students",
-  },
-];
+export const schoolList = (usertype: string, id: number) => {
+  return [
+    {
+      list: 'پروفایل کاربری',
+      icon: <PersonAddAlt1Icon />,
+      to: `/${usertype}/${id}`,
+    },
+    {
+      list: 'لیست دانشجویان',
+      icon: <PersonSharpIcon />,
+      to: '/list/students',
+    },
+  ];
+};
 
-export const teacherList = [
-  {
-    list: "لیست مدارس",
-    icon: <SchoolIcon />,
-    to: "/list/schools",
-  },
-  {
-    list: "لیست دانشجویان",
-    icon: <PersonSharpIcon />,
-    to: "/list/students",
-  },
-];
+export const teacherList = (usertype: string, id: number) => {
+  return [
+    {
+      list: 'پروفایل کاربری',
+      icon: <PersonAddAlt1Icon />,
+      to: `/${usertype}/${id}`,
+    },
+    {
+      list: 'لیست مدارس',
+      icon: <SchoolIcon />,
+      to: '/list/schools',
+    },
+    {
+      list: 'لیست دانشجویان',
+      icon: <PersonSharpIcon />,
+      to: '/list/students',
+    },
+  ];
+};
+export const professorList = (usertype: string, id: number) => {
+  return [
+    {
+      list: 'پروفایل کاربری',
+      icon: <PersonAddAlt1Icon />,
+      to: `/${usertype}/${id}`,
+    },
+    {
+      list: 'لیست دانشجویان',
+      icon: <PersonSharpIcon />,
+      to: '/list/students',
+    },
+  ];
+};
 
-export const professorList = [
-  {
-    list: "لیست دانشجویان",
-    icon: <PersonSharpIcon />,
-    to: "/list/students",
-  },
-];
-
-export const studentList = [
-  {
-    list: "درخواست ها",
-    icon: <PersonAddAlt1Icon />,
-    to: "/requests",
-  },
-];
+export const studentList = (usertype: string, id: number) => {
+  return [
+    {
+      list: 'پروفایل کاربری',
+      icon: <PersonAddAlt1Icon />,
+      to: `/${usertype}/${id}`,
+    },
+  ];
+};
