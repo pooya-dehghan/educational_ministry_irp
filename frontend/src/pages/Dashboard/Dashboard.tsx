4f427a38cb1e4fd2023d8913e5c27e7db4f10881
pooya_dehghan <pooyadehghan@yahoo.com>
2023-10-05 18:29:24 +0330

    delted setting section in header

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,32 +1,32 @@
-import * as React from "react";
-import { Divider, Grid } from "@mui/material";
-import { Box } from "@mui/system";
-import { useState, useEffect, ReactNode } from "react";
-import styles from "./Dashboard.module.css";
-import SideBar from "../../components/SideBar/SideBar";
-import { useDispatch, useSelector } from "react-redux";
-import { dashboardAsync } from "../../features/dashboard/dashboardThunk";
-import { RootState } from "../../store/store";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import { useNavigate } from "react-router-dom";
-import LogoutIcon from "@mui/icons-material/Logout";
-import SettingsIcon from "@mui/icons-material/Settings";
-import CustomBadge from "../../components/Badge/Badge";
-import Notification from "../Notifications/Notifications";
-import { getAllNotificationsAsync } from "../../features/notifications/notificationThunk";
-import { logout } from "../../features/auth/authSlice";
-import * as tokenHandler from "../../utils/token/index";
-import * as userInfoLocalStorage from "../../utils/storageUser/index";
-import { updateResponse } from "../../features/response/responseSlice";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import * as React from 'react';
+import { Divider, Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import { useState, useEffect, ReactNode } from 'react';
+import styles from './Dashboard.module.css';
+import SideBar from '../../components/SideBar/SideBar';
+import { useDispatch, useSelector } from 'react-redux';
+import { dashboardAsync } from '../../features/dashboard/dashboardThunk';
+import { RootState } from '../../store/store';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { useNavigate } from 'react-router-dom';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CustomBadge from '../../components/Badge/Badge';
+import Notification from '../Notifications/Notifications';
+import { getAllNotificationsAsync } from '../../features/notifications/notificationThunk';
+import { logout } from '../../features/auth/authSlice';
+import * as tokenHandler from '../../utils/token/index';
+import * as userInfoLocalStorage from '../../utils/storageUser/index';
+import { updateResponse } from '../../features/response/responseSlice';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 interface PageWrapper {
   children?: ReactNode;
@@ -44,9 +44,9 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
   const [notificationOpen, setNotificationOpen] = useState<boolean>(false);
   const [notifications, setNotifications] = useState([]);
   const [userInfo, setUserInfo] = useState({
-    username: "وارد نشده است",
+    username: 'وارد نشده است',
     id: 0,
-    type: "وارد نشده است",
+    type: 'وارد نشده است',
   });
   const navigate = useNavigate();
   const [imgNum, setImgNum] = useState(0);
@@ -96,7 +96,7 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
   };
 
   const profileClick = () => {
-    navigate("/dashboard/profile");
+    navigate('/dashboard/profile');
   };
 
   const logOutClick = () => {
@@ -106,32 +106,32 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
     dispatch(logout());
     dispatch(
       updateResponse({
-        severity: "warning",
-        message: "شما با موفقیت از سامانه خارج شدید.",
+        severity: 'warning',
+        message: 'شما با موفقیت از سامانه خارج شدید.',
         open: true,
       })
     );
-    navigate("/login");
+    navigate('/login');
   };
 
   const imageDataSet: ImageData[] = [
     {
-      name: "Image 0",
+      name: 'Image 0',
       imageUrl:
-        "https://uploadkon.ir/uploads/4d9504_2336985c2cbca64ee7b8eb5ba048c7bd92-ei-1649215445587.jpg",
+        'https://uploadkon.ir/uploads/4d9504_2336985c2cbca64ee7b8eb5ba048c7bd92-ei-1649215445587.jpg',
     },
     {
-      name: "Image 1",
-      imageUrl: "https://uploadkon.ir/uploads/0d8804_23Daneshgah-Rajaee1.jpg",
+      name: 'Image 1',
+      imageUrl: 'https://uploadkon.ir/uploads/0d8804_23Daneshgah-Rajaee1.jpg',
     },
     {
-      name: "Image 2",
-      imageUrl: "https://uploadkon.ir/uploads/009f04_23Srttu-winter-1394.jpg",
+      name: 'Image 2',
+      imageUrl: 'https://uploadkon.ir/uploads/009f04_23Srttu-winter-1394.jpg',
     },
     {
-      name: "Image 3",
+      name: 'Image 3',
       imageUrl:
-        "https://uploadkon.ir/uploads/6eae04_23دانشگاه-تربیت-دبیر-شهید-رجایی.png",
+        'https://uploadkon.ir/uploads/6eae04_23دانشگاه-تربیت-دبیر-شهید-رجایی.png',
     },
   ];
 
@@ -147,7 +147,7 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
   };
   return (
     <>
-      <Box component={"div"} className={styles.container}>
+      <Box component={'div'} className={styles.container}>
         <SideBar
           open={drawerOpen}
           handleDrawerToggle={() => setDrawerOpen(!drawerOpen)}
@@ -165,7 +165,7 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
                   <MenuIcon />
                 </IconButton>
 
-                <Typography sx={{ flexGrow: 1, textAlign: "right" }}>
+                <Typography sx={{ flexGrow: 1, textAlign: 'right' }}>
                   {`کاربر گرامی : ${userInfo.username} `}
                 </Typography>
                 {auth && (
@@ -184,13 +184,13 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
                       id="menu-appbar"
                       anchorEl={anchorEl}
                       anchorOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
+                        vertical: 'top',
+                        horizontal: 'right',
                       }}
                       keepMounted
                       transformOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
+                        vertical: 'top',
+                        horizontal: 'right',
                       }}
                       open={Boolean(anchorEl)}
                       onClose={handleClose}
@@ -198,10 +198,6 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
                       <MenuItem onClick={accountClick}>
                         <AccountCircle />
                         حساب کاربری
-                      </MenuItem>
-                      <MenuItem onClick={profileClick}>
-                        <SettingsIcon />
-                        تنظیمات
                       </MenuItem>
                       <MenuItem onClick={logOutClick}>
                         <LogoutIcon />
@@ -219,7 +215,7 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
               </Toolbar>
             </AppBar>
           </Grid>
-          <Grid container direction={"column"}>
+          <Grid container direction={'column'}>
             <Grid item>{children}</Grid>
           </Grid>
           <Grid>
@@ -239,20 +235,20 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
         >
           <Box
             sx={{
-              display: "flex",
-              backgroundColor: "white",
-              width: "1000px",
-              height: "450px",
-              marginTop: "50px",
-              boxShadow: "5px 5px 10px",
-              "@media (max-width:1000px)": {
-                width: "90%",
+              display: 'flex',
+              backgroundColor: 'white',
+              width: '1000px',
+              height: '450px',
+              marginTop: '50px',
+              boxShadow: '5px 5px 10px',
+              '@media (max-width:1000px)': {
+                width: '90%',
               },
-              "@media (max-width: 550px)": {
-                flexDirection: "column",
-                justifyContent: "center",
-                width: "90%", // Full width on mobile
-                height: "auto", // Auto height on mobile
+              '@media (max-width: 550px)': {
+                flexDirection: 'column',
+                justifyContent: 'center',
+                width: '90%', // Full width on mobile
+                height: 'auto', // Auto height on mobile
               },
             }}
           >
@@ -262,22 +258,22 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
               xs={12}
               sm={6}
               sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                backgroundColor: "black",
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                backgroundColor: 'black',
               }}
             >
               <img
-                style={{ height: "410px", width: "500px", maxWidth: "100%" }}
+                style={{ height: '410px', width: '500px', maxWidth: '100%' }}
                 src={imageDataSet[imgNum].imageUrl}
               />
 
-              <div style={{ display: "flex" }}>
-                <IconButton sx={{ color: "white" }} onClick={nextIconClick}>
+              <div style={{ display: 'flex' }}>
+                <IconButton sx={{ color: 'white' }} onClick={nextIconClick}>
                   <ArrowForwardIosIcon />
                 </IconButton>
-                <IconButton sx={{ color: "white" }} onClick={backIconClick}>
+                <IconButton sx={{ color: 'white' }} onClick={backIconClick}>
                   <ArrowBackIosIcon />
                 </IconButton>
               </div>
@@ -287,10 +283,10 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
               item
               xs={12}
               sm={6}
-              sx={{ backgroundColor: "rgb(150,237,237)" }}
+              sx={{ backgroundColor: 'rgb(150,237,237)' }}
             >
               <Grid item margin="30px" marginBottom="0px">
-                <h3 style={{ textAlign: "center" }}>
+                <h3 style={{ textAlign: 'center' }}>
                   کاربر گرامی، به سامانه مدیریت کارورزی دانشگاه شهید رجایی خوش
                   آمدید
                 </h3>
@@ -299,29 +295,29 @@ const Dashboard: React.FC<PageWrapper> = ({ children }) => {
                 item
                 xs={12}
                 sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
                 }}
               >
                 <Divider
                   sx={{
-                    backgroundColor: "black",
-                    width: "90%",
+                    backgroundColor: 'black',
+                    width: '90%',
                   }}
                 />
               </Grid>
               <Grid display="flex" alignItems="center" justifyContent="center">
                 <Box
                   sx={{
-                    width: "80%",
-                    height: "auto",
-                    borderRadius: "10px",
-                    border: "1px solid black",
-                    padding: "10px",
-                    margin: "30px",
-                    textAlign: "center",
-                    backgroundColor: "red",
+                    width: '80%',
+                    height: 'auto',
+                    borderRadius: '10px',
+                    border: '1px solid black',
+                    padding: '10px',
+                    margin: '30px',
+                    textAlign: 'center',
+                    backgroundColor: 'red',
                   }}
                 >
                   <Typography color="white">
